5e997a76682d04c3233f83118502640a8652b212
rikicop <ruperto1@protonmail.com>
2021-10-15 18:23:11 -0400

    Back and End

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("La Api está activa...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Servidor activo en el Puerto ${PORT}`));
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+const App = () => {
+  const hello = "Ricardo";
+
+  return <div className="App">Hello!! {hello} </div>;
+};
+
+export default App;
